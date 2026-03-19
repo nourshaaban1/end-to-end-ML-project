09ae03fdceda03096e9adde8c78a6c5f3a367bc3
nourshaaban <nourshaaban59@gmail.com>
2026-03-19 04:18:04 +0200

    Implement initial project structure including ML pipeline components, utilities, and configuration.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+def get_requirements(path):
+    requirements = []
+    with open(path) as file:
+        requirements = file.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+            
+    return requirements
+
+setup(
+    name = 'mlproject',
+    version = '0.0.1',
+    author = 'Nour Shaaban',
+    author_email = ['nourshaaban59@gmail.com'],
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
